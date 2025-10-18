cebb75cde0e906de472e2a0e651dc8c65990f466
Harry Nguyen <70305505+huynxtb@users.noreply.github.com>
2025-10-18 12:56:19 +0700

    feat: create Two Sum solution

---
+++ Easy/Two Sum/two-sum.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> numMap = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+            if (numMap.ContainsKey(complement)) {
+                return new int[] { numMap[complement], i };
+            }
+            numMap[nums[i]] = i;
+        }
+        return new int[0]; // Should not be reached based on problem constraints
+    }
+}
